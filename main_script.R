8bf052d6f19fe9a47e52fba10404e7eb41530188
mpourossell <mpourossell@gmail.com>
2024-02-06 11:49:51 +0100

    added setosa subsetting data

--- main_script.R
+++ main_script.R
@@ -1,4 +1,10 @@
 library(dplyr)
 library(ggplot2)
+library(readr)
 
-iris_data <- iris
+# reading data
+iris_data <- read_csv('raw_data/iris.csv')
+
+# subsetting data
+setosa_subset <- iris_data |>
+  filter(Species == "setosa")
